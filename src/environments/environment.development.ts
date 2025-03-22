431e15128aede17689a1791033ff39828b185cb3
Johan <Johandutoit139@gmail.com>
2025-03-22 15:43:03 +0200

    connection string change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  BACKEND_URL: 'https://probuildai-backend.wonderfulgrass-0f331a8e.centralus.azurecontainerapps.io/api',
+  BACKEND_URL: 'https://probuildai-backend.internal.wonderfulgrass-0f331a8e.centralus.azurecontainerapps.io/api',
   API_KEY: 'ef306472fbed4ca9835115255241412',
   production: true
 };
